c973650b050d0041a6cab448e3c7d09eea3163e4
WebMohit20 <2003mohitkhemani@gmail.com>
2024-09-20 18:52:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+    const getSumBtn = document.createElement("button");
+    getSumBtn.append("Get Total Price");
+    document.body.appendChild(getSumBtn);
+    let price = document.querySelectorAll(".price");
+    let sum;
+    let table = document.querySelector("table");
+    const getSum = () => {
+    //Add your code here
+        sum=0
+        for(let t of price){
+            sum+=(t.innerHTML)-0
+        }
+        let tr = document.createElement("tr");
+        table.append(tr);
+        let td = document.createElement("td");
+        // tr.append(td);
+        td.innerHTML = sum;
+        tr.append(td);
+    };
 
-const getSum = () => {
-//Add your code here
-  
-};
-
-getSumBtn.addEventListener("click", getSum);
+    getSumBtn.addEventListener("click", getSum);
 
